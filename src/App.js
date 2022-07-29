0b6c37db94ad8ddf9ad4562d0894b22fe2597053
Stdcop89dv <62072080+Stdcop89dv@users.noreply.github.com>
2022-07-29 15:38:22 -0500

    layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import LayoutComp from './components/layout';
+
 import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>React App</h2>
+      <LayoutComp />
     </div>
   );
 }
